763ebc74057247cccf1ee2b90c9240ddd2698187
Harsh Gupta <harsh.gupta@cognustechnology.com>
2023-12-30 17:58:52 +0530

    new pages added

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -15,16 +15,27 @@ class Welcome extends CI_Controller {
 		$portfolio['name'] = 'Harsh Gupta';
 		$portfolio['title'] = 'Experienced Flutter Developer';
 		$portfolio['subtitle'] = 'I build exceptional and accessible digital experiences for the Android.';
-		$portfolio['overview'] = 'I build exceptional and accessible digital experiences for the Android.';
+		$portfolio['about'] = 'I build exceptional and accessible digital experiences for the Android.';
 		$portfolio['experience'] = array(
-			'time'=>'about',
-			'position'=>'experience',
-			'company'=>'projects',
-			'company_url'=>'projects',
-			'technologies'=>'projects',
-			'projects'=>'projects',
-			'projects_url'=>'projects',
-		);		
+			array(
+				'time' => 'about',
+				'position' => 'experience',
+				'company' => 'projects',
+				'company_url' => 'projects',
+				'technologies' => 'projects',
+				'projects' => 'projects',
+				'projects_url' => 'projects',
+			),
+			array(
+				'time' => 'about',
+				'position' => 'experience',
+				'company' => 'projects',
+				'company_url' => 'projects',
+				'technologies' => 'projects',
+				'projects' => 'projects',
+				'projects_url' => 'projects',
+			)
+		);
 		// echo "<pre>";
 		// print_r($portfolio);
 		// die;
@@ -32,8 +43,3 @@ class Welcome extends CI_Controller {
 	}
 		
 }
-// $portfolio['tabs'] = array(
-// 	'about'=>'about',
-// 	'experience'=>'experience',
-// 	'projects'=>'projects',
-// );
